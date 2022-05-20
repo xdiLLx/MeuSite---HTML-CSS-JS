799039a8ea1d5bb8f4e6a3dcb3dbc8146df2c98b
Gabriel Dill <98971249+xdiLLx@users.noreply.github.com>
2022-05-20 13:18:19 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function abrirsite(tipo)
+ {
+    if (tipo==1){
+   window.open('https://google.com.br', '_blank');
+    }
+    if (tipo==2){
+      window.open('https://www.instagram.com/gabrieldill_/', '_blank');
+       }
+       if (tipo==3){
+         window.open('https://github.com/xdiLLx', '_blank');
+          }
+ }
